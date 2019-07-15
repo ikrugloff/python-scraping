cc9dad1a3b24a57aaaf4d479d28a0799c13b6e8e
ilya.kruglov <ilya.kruglov@megafon.ru>
2019-07-15 16:20:29 +0300

    Small fix in chapter 3

--- v1/chapter3/1-getWikiLinks.py
+++ v1/chapter3/1-getWikiLinks.py
@@ -4,13 +4,33 @@ import datetime
 import random
 import re
 
+
+"""p.53, final code"""
 random.seed(datetime.datetime.now())
+
+
 def getLinks(articleUrl):
     html = urlopen("http://en.wikipedia.org"+articleUrl)
     bsObj = BeautifulSoup(html, "html.parser")
     return bsObj.find("div", {"id":"bodyContent"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$"))
+
+
 links = getLinks("/wiki/Kevin_Bacon")
 while len(links) > 0:
     newArticle = links[random.randint(0, len(links)-1)].attrs["href"]
     print(newArticle)
     links = getLinks(newArticle)
+
+# """p.51, 1st iteration"""
+# html = urlopen("http://en.wikipedia.org/wiki/Kevin_Bacon")
+# bsObj = BeautifulSoup(html, "html.parser")
+# for link in bsObj.find_all("a"):
+#     if "href" in link.attrs:
+#         print(link.attrs["href"])
+
+# """p.52, 2nd iteration"""
+# html = urlopen("http://en.wikipedia.org/wiki/Kevin_Bacon")
+# bsObj = BeautifulSoup(html, "html.parser")
+# for link in bsObj.find("div", {"id": "bodyContent"}).findAll("a", href=re.compile("(/wiki/)((?!:).)*$")):
+#     if "href" in link.attrs:
+#         print(link.attrs["href"])
