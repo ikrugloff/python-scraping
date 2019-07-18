627b374e4d4beb5da8d3b1cd3ae13937d3248c66
ilya.kruglov <ilya.kruglov@megafon.ru>
2019-07-18 11:41:19 +0300

    LOG mods

--- v1/chapter3/scrapy/wikiSpider/wikiSpider/settings.py
+++ v1/chapter3/scrapy/wikiSpider/wikiSpider/settings.py
@@ -14,7 +14,7 @@ BOT_NAME = 'wikiSpider'
 SPIDER_MODULES = ['wikiSpider.spiders']
 NEWSPIDER_MODULE = 'wikiSpider.spiders'
 
-LOG_LEVEL = 'ERROR'
+LOG_LEVEL = 'ERROR'  # Pretty output
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
